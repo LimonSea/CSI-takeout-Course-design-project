381b5b8b8505c5ad998945fc16905f6f34e457c6
LimonSea <714034744@qq.com>
2019-07-02 14:31:21 +0800

    管理员页面和用户页面初步完成

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const global = {
   // targetUrl: 'http://192.168.0.107:8081/test',
-  targetUrl:'http://yanfucheng.free.idcfengye.com/test'
+  // targetUrl:'http://yanfucheng.free.idcfengye.com/'
+  targetUrl:'http://10.203.1.13/'
 }
