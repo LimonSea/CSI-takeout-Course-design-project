ef5da460fc4d8f2f5f908d0fbd250022a6205571
LimonSea <714034744@qq.com>
2019-07-03 14:25:55 +0800

    初步完成版本

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 const global = {
   // targetUrl: 'http://192.168.0.107:8081/test',
-  // targetUrl:'http://yanfucheng.free.idcfengye.com/'
-  targetUrl:'http://10.203.1.13/'
+  // targetUrl:'http://yanfucheng.free.idcfengye.com:8081/'
+  targetUrl:'http://10.203.1.13/luntan/',
+  imageUrl:'http://10.203.1.13/image/'
 }
+
+// const logOut = () => {
+//   localStorage.clear()
+//   location.href('login.html')
+// }
